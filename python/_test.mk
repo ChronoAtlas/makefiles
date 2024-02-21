ee4c856d4046c0d7bd7b8ac9d53a33f08c277b7c
Andreas <andreas.kruehlmann@siemens.com>
2024-02-21 18:05:00 +0100

    Add log level to pytest

--- python/_test.mk
+++ python/_test.mk
@@ -3,7 +3,7 @@ test: unit-test integration-test
 
 .PHONY: unit-test
 unit-test: $(VERSION)
-	-pytest tests/unit_tests
+	-pytest tests/unit_tests --log-cli-level=INFO
 	ret=$$?
 	test "$$ret" = 5 && ret=0
 	exit $$ret
@@ -11,7 +11,7 @@ unit-test: $(VERSION)
 .PHONY: integration-test
 integration-test: $(VERSION)
 	docker-compose -f tests/docker-compose.yaml up -d
-	-pytest tests/integration_tests
+	-pytest tests/integration_tests --log-cli-level=INFO
 	ret=$$?
 	docker-compose -f tests/docker-compose.yaml down
 	test "$$ret" = 5 && ret=0
