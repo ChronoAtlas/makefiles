21ae311498f3ed96c34697dddba565b83b4ecc64
Andreas <andreas.kruehlmann@siemens.com>
2024-03-01 17:56:01 +0100

    Add haskell

--- haskell/main.mk
+++ haskell/main.mk
@@ -1,11 +1,7 @@
 .ONESHELL:
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-.PHONY: all
-all: $(COMPONENT)
-
+include $(SELF_DIR)/_build.mk
 include $(SELF_DIR)/_install.mk
 include $(SELF_DIR)/_lint.mk
-
-$(COMPONENT):
-	dune build
+include $(SELF_DIR)/_test.mk
--- ocaml/_lint.mk
+++
@@ -1,6 +0,0 @@
-.PHONY: lint
-lint:
-	ocamlformat --check $$(find . -name '*.ml' -type f)
-
-fix:
-	ocamlformat --inplace $$(find . -name '*.ml' -type f)
