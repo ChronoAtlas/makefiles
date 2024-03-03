094842e96846d294ac0ec7fa15ddc05f0017b302
Andreas Krühlmann <andreas@kruhlmann.dev>
2024-03-03 20:54:19 +0100

    Add manpage

--- ocaml/main.mk
+++ ocaml/main.mk
@@ -2,8 +2,11 @@
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 .PHONY: all
-all: 
+all: bin/$(COMPONENT).1
 	dune build
+
+bin/$(COMPONENT).1:
+	dune exec bin/main.exe -- --help=groff >$@
 
 include $(SELF_DIR)/_lint.mk
 include $(SELF_DIR)/_test.mk
